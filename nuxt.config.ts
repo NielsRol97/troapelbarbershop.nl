bbda09225008384c01960b28a6847b962cee3282
Niels Rol <niels@example.com>
2026-01-14 17:06:46 +0100

    Removed BaseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,4 @@ export default defineNuxtConfig({
   css: ['@/assets/css/fonts.css','@/assets/css/variables.css','@/assets/css/main.css'],
   modules: ['@nuxt/fonts', '@nuxt/icon'],
   ssr: false,
-  app: {
-    baseURL: '/troapelbarbershop.nl/'
-  }
 })
