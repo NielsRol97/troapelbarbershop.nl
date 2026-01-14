2b8b77e1d0cb10d5c69a9ecf77181cea7d995a09
Niels Rol <niels@example.com>
2026-01-14 17:34:33 +0100

    assetbuilder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   css: [
     '@/assets/css/fonts.css',
     '@/assets/css/variables.css',
     '@/assets/css/main.css'
   ],
+
   modules: ['@nuxt/fonts', '@nuxt/icon'],
   ssr: false,
 
   app: {
     baseURL: '/troapelbarbershop.nl/',
-    buildAssetsDir: '_nuxt/' // ← force absolute asset paths
+    buildAssetsDir: '_nuxt/'
   }
 })
