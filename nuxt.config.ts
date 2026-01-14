ddf46e51067c89deacf81215d2065fcb10a148bd
Niels Rol <niels@example.com>
2026-01-14 17:00:23 +0100

    Config edit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['@/assets/css/fonts.css','@/assets/css/variables.css','@/assets/css/main.css'],
-  modules: ['@nuxt/fonts', '@nuxt/icon']
+  modules: ['@nuxt/fonts', '@nuxt/icon'],
+  ssr: false,
+  app: {
+    baseURL: '/troapelbarbershop.nl/'
+  }
 })
