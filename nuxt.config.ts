df5808148bdbe9f65d507b29ff739d19ec658b80
Niels Rol <niels@example.com>
2026-01-14 17:24:03 +0100

    BaseURL reimplemented

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['@/assets/css/fonts.css','@/assets/css/variables.css','@/assets/css/main.css'],
+  css: [
+    '@/assets/css/fonts.css',
+    '@/assets/css/variables.css',
+    '@/assets/css/main.css'
+  ],
   modules: ['@nuxt/fonts', '@nuxt/icon'],
   ssr: false,
+  app: {
+    baseURL: '/troapelbarbershop.nl/'
+  }
 })
